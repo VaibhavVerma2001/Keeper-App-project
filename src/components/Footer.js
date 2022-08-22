41c0b4f861c39641af455f714e95454646c05cfa
Vaibhav <vaibhavverma21122001@gmai.com>
2022-08-22 15:52:10 +0530

    components

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer()
+{
+    let date = new Date();
+    let year = date.getFullYear();
+    return (
+    <>
+    <footer>
+        <p>Copyright @{year}</p>
+    </footer>
+    </>
+    );
+}
+
+export default Footer;
