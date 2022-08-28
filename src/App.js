143e6df62b0fbe33f6d9569e83697154f228c4b3
Vaibhav <vaibhavverma21122001@gmai.com>
2022-08-28 11:46:51 +0530

    mapping data to components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './css/App.css';
 import Footer from './components/Footer';
 import Header from './components/Header'
 import Note from './components/Note'
+import notes from './notes';
+
+function createNotes (noteItem)
+{
+  return (
+    <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />
+  );
+}
 
 function App() {
   return (
     <>
       <Header />
 
-      <Note />
+      {notes.map(createNotes)};
 
       <Footer />
     </>
